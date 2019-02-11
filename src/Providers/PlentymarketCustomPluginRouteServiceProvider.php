b997dd51ac6b9369e40f5be17a8c51916c99742a
optimumprotect <47454033+optimumprotect@users.noreply.github.com>
2019-02-11 14:19:26 +0530

    create route

---
+++ src/Providers/PlentymarketCustomPluginRouteServiceProvider.php
@@ -0,0 +1,15 @@
+<?php
+ 
+namespace PlentymarketCustomPlugin\Providers;
+ 
+ 
+use Plenty\Plugin\RouteServiceProvider;
+use Plenty\Plugin\Routing\Router;
+ 
+class PlentymarketCustomPluginRouteServiceProvider extends RouteServiceProvider
+{
+    public function map(Router $router)
+    {
+ 
+    }
+}
