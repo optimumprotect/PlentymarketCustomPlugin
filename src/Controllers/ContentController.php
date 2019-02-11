81ed44554d81aca80eafe3b7b497b0c4698873bd
optimumprotect <47454033+optimumprotect@users.noreply.github.com>
2019-02-11 14:27:21 +0530

    connect

--- src/Controllers/ContentController.php
+++ src/Controllers/ContentController.php
@@ -10,6 +10,6 @@ class ContentController extends Controller
 {
     public function sayHello(Twig $twig):string
     {
-        return $twig->render('PlentymarketCustomPlugin::TEMPLATE');
+        return $twig->render('PlentymarketCustomPlugin::content.hello');
     }
 }
